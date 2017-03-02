e3375d0b007de29540e26046588ccd67661a1610
Alessandro Marzialetti <amarzial@student.42.fr>
2017-03-02 17:17:47 +0100

    header fix

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: abonneca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/02 13:39:59 by abonneca          #+#    #+#             */
-/*   Updated: 2017/03/02 16:22:30 by amarzial         ###   ########.fr       */
+/*   Updated: 2017/03/02 17:17:34 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,11 +39,12 @@ typedef	struct	s_vm
 
 struct			s_champion
 {
-	char	*name;
-	char	*comment;
-	int		id;
-	t_list	*procs;
-	size_t	procs_n;
+	char			*name;
+	char			*comment;
+	unsigned int	id;
+	int				fd;
+	t_list			*processes;
+	size_t			process_n;
 }				t_champion;
 
 typedef struct	s_process
@@ -53,15 +54,7 @@ typedef struct	s_process
 	int		carry:1;
 	t_op	*current;
 	int		cycle_count;
-
 }				t_process;
-
-typedef struct	s_player
-{
-	t_process		*process;
-	struct s_player	*next;
-
-}				t_player;
 
 t_vm			*vm_get()
 int				*vm_init()
