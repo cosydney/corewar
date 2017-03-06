e419f8ec135415bad898a30ed07a84756f0627e0
cosydney <chnsydney@gmail.com>
2017-03-06 17:58:51 +0100

    ""

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: sycohen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 18:22:17 by sycohen           #+#    #+#             */
-/*   Updated: 2017/03/04 16:32:03 by sycohen          ###   ########.fr       */
+/*   Updated: 2017/03/06 17:54:54 by sycohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,39 @@
 # define LABEL_CHAR					':'
 # define DIRECT_CHAR				'%'
 # define SEPARATOR_CHAR				','
+
+# define FORMAT_ERROR				1
+# define COMMENT_ERROR				2
+# define LABELS_ERROR				3
+# define LABEL_CHAR_ERROR			4
+# define INSTRUCT_ERROR				5
+# define MALLOC_ERROR				6
+# define INDIRECT_ERROR				7
+# define ARGS_ERROR					8
+# define LABELS_DOUBLE_ERROR		9
+# define LABEL_MATCH_ERROR			10
+# define NAME_ERROR					11
+# define ARG_ERROR					12
+# define LINES_ERROR				13
+# define FILE_ERROR					14
+# define LABEL_ERROR				15
+
+# define LIVE						1
+# define LD							2
+# define ST							3
+# define ADD						4
+# define SUB						5
+# define AND						6
+# define OR							7
+# define XOR						8
+# define ZJMP						9
+# define LDI						10
+# define STI						11
+# define FORK						12
+# define LLD						13
+# define LLDI						14
+# define LFORK						15
+# define AFF						16
 
 #define LABEL_CHARS					"abcdefghijklmnopqrstuvwxyz_0123456789"
 
@@ -55,5 +88,12 @@ int			name_comment_handler(int fd, t_header *head);
 int			check_header(t_header *head, char *line);
 int			check_label(char *str);
 t_label		*label_init(t_label *label, char *line);
+int			check_valid_line(char *line);
+int			asm_instruct_name(char *line);
+char		*asm_free_join(char *line, char *file);
+int			asm_error(int error);
+int			check_arg(char **line, int op);
+int			check_direct(char **line, int op, int check);
+
 
 #endif
