fbbac680e0ec6398a576cd2981655c7c369e331e
Alessandro Marzialetti <setti93@gmail.com>
2017-03-11 19:27:06 +0100

    fixing ops

---
+++ srcs/op_funcs.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op_funcs.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/11 18:42:05 by amarzial          #+#    #+#             */
+/*   Updated: 2017/03/11 18:44:13 by amarzial         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+unsigned int	par_to_val(int par, size_t size, t_process *proc, t_vm *vm)
+{
+	t_param *p;
+
+	p = &(proc->act.params[par]);
+	if (p->t == T_REG)
+		return (regtou(proc->registers[regtou(p->value)]));
+	else if (p->t == T_DIR)
+		return (regtou(p->value));
+	else if (p->t == T_IND)
+		return (memtou(vm->memory, (regtou(p->value) + regtou(proc->pc)), \
+				size));
+	return (0);
+}
+
+
